3922994fc46d66abb4fd607bef2257cd7c8c7453
Jin Ke <jinkebj@126.com>
2017-08-28 15:28:15 +0800

    add build script for release HandyNote-Service & HandyNote-Web

--- README.md
+++ README.md
@@ -2,12 +2,31 @@
 
 HandyNote project contain the following independent repository:
 
-- HandyNote - Overall info for HandyNote project
+- HandyNote - Overall info & release wrapper for HandyNote project
 
 - HandyNote-Service - Provide restful/streaming API for HandyNote-Web, HandyNote-Mobile & HandyNote-Desktop
 
-- HandyNote-Web - HandyNote Web UI
+- HandyNote-Web - HandyNote Web App
 
 - HandyNote-Mobile - HandyNote Mobile App, support Android & iOS
 
 - HandyNote-Desktop - HandyNote Desktop App, support Mac / Linux / Windows
+
+## Quick Start
+
+``` bash
+# Optional, set npm mirror to speed up npm install in China
+npm config set registry https://registry.npm.taobao.org
+
+# install dependencies
+npm install
+
+# build release version for HandyNote-Service & HandyNote-Web
+npm run build
+
+# start HandyNote-Service & HandyNote-Web in production mode
+npm start
+
+# stop HandyNote-Service & HandyNote-Web
+npm stop
+```
---
+++ build.js
@@ -0,0 +1,53 @@
+var fs = require('fs')
+var shell = require('shelljs')
+
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git')
+  shell.exit(1);
+}
+
+console.log('=== start build for HandyNote ===')
+
+// check HandyNote-Service & HandyNote-Web folder
+shell.cd('..')
+if (!fs.existsSync('HandyNote-Service')) {
+  shell.echo('HandyNote-Service folder does not exist! Download it from github...')
+  if (shell.exec('git clone git@github.com:jinkebj/HandyNote-Service.git').code !== 0) {
+    shell.echo('Error: Get HandyNote-Service from github failed');
+    shell.exit(1);
+  }
+}
+
+if (!fs.existsSync('HandyNote-Web')) {
+  shell.echo('HandyNote-Web folder does not exist! Download it from github...')
+  if (shell.exec('git clone git@github.com:jinkebj/HandyNote-Web.git').code !== 0) {
+    shell.echo('Error: Get HandyNote-Web from github failed');
+    shell.exit(1);
+  }
+}
+
+// prepare dist dir
+shell.rm('-rf', 'HandyNote/dist')
+shell.mkdir('-p', 'HandyNote/dist')
+
+// build HandyNote-Service
+console.log('=== start build HandyNote-Service ===')
+shell.cd('HandyNote-Service')
+if (shell.exec('npm install && npm run build').code !== 0) {
+  shell.echo('Error: build HandyNote-Service failed')
+  shell.exit(1)
+}
+shell.cp('dist/app.bundle.js', '../HandyNote/dist/hnservice.js')
+shell.cd('..')
+console.log('=== finish build HandyNote-Service ===')
+
+// build HandyNote-Web
+console.log('=== start build HandyNote-Web ===')
+shell.cd('HandyNote-Web')
+if (shell.exec('npm install && npm run build').code !== 0) {
+  shell.echo('Error: build HandyNote-Web failed')
+  shell.exit(1)
+}
+shell.cp('-r', 'dist/', '../HandyNote/dist/hnweb')
+shell.cd('..')
+console.log('=== finish build HandyNote-Web ===')
