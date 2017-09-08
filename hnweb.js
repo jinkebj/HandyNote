e35e2cced98c57849764cd70df163faf76769837
Jin Ke <jinkebj@126.com>
2017-09-08 13:02:53 +0800

    use koa-static-cache instead of koa-static for better static assets cache

--- hnweb.js
+++ hnweb.js
@@ -1,5 +1,7 @@
 const SERVER_PORT = 9080
+const ROOT_PATH = __dirname + '/hnweb'
 const Koa = require('koa')
 const app = new Koa()
-app.use(require('koa-static')(__dirname + '/hnweb', {gzip: true}))
+app.use(require('koa-static-cache')(ROOT_PATH, {gzip: true, maxAge: 30 * 24 * 60 * 60}))
+app.use(async (ctx, next) => require('koa-send')(ctx, '/index.html', {root: ROOT_PATH}).then(() => next()))
 app.listen(SERVER_PORT, () => console.log(new Date() + ' - Server started at port: ' + SERVER_PORT))
